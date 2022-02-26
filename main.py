2f2e20ef41d416f5c22c0906ab01b9cb7e1c972f
Lucas Munson <lucasbmunson@gmail.com>
2022-02-25 21:36:38 -0500

    adjusted channel target for bot

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    words = message.content.split()
-    score = words[2]
+    wordle_channel = config.CHANNEL_ID  
+    if str(message.channel.id) == str(wordle_channel):
+        words = message.content.split()
+        score = words[2]
 
-    if int(score[0]) < 5:
-        await message.pin()
+        if int(score[0]) < 5:
+            await message.pin()
 
 
 client.run(config.TOKEN)
